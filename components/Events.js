b0e2db4ef481e2b7df3e55ab004a1d089c235a32
lexifdev <sl@lxf.kr>
2020-04-01 08:31:08 +0900

    사례 업데이트

--- components/Events.js
+++ components/Events.js
@@ -1,5 +1,6 @@
 import React from 'react'
 import Ticker from 'react-ticker'
+import layout from 'pages/layout.styl'
 import style from './Event.styl'
 
 
@@ -29,40 +30,63 @@ const Raw = ({raw}) => (
 
 export default () => {
     const events = [{
-        quote: `귀가 중이던 여성을 뒤쫓아가 집에 침입하려 한 신림동 스토커 사건`,
-        event: `강간미수 불인정`
+        quote: `피해자가 아프다며 밀어내는 행동을 했다고 하였으나 폭행 또는 협박으 로 반항을 억압했다고 인정하기에는 부족하다`,
+        event: `강간 무죄, 보습학원장에 의한 10세 아동 성폭력 사건(2심)`
     }, {
-        quote: `세계 최대 아동 성폭력 비디오 웰컴투 비디오 사건`,
-        event: `징역 1년 6개월`
+        quote: `피고인이 피해자의 항거를 불가능하게 하거나 현저히 곤란하게 한 상태 에서 옷을 벗겼다고 보기는 어렵다 (...) 피해자가 객관적으로는 반항하거나 도망을 갈 수 있는 상황이었는데 주관적으로 더 큰 일이 일어날까봐 무섭게 생각하고 당황스러워 그렇게 못했다`,
+        event: `강간 및 강제추행 무죄, 삼촌에 의한 친족 성폭력 사건(2심)`
     }, {
-        quote: `고기를 덜어준 호의를 '암묵적 성관계 동의'라고 해석 감자탕 성폭행 사건 `,
-        event: `강간 무죄`
-    }]
-
-    const quotes = [{
-        quote: `강간할 의도를 가지고 있었다고 인정하려면 폭행이나 협박을 가한 정황이 입증돼야 하지만, 이를 인정할 증거가 부족하다.`,
-        event: `신림동 스토커 사건`
+        quote: `성관계 등 사진은 폭행·협박 사실에 대한 직접증거가 될 수 없고, (...) 강간행위와 그 고의를 입증할 증거를 발견할 수 없었다`,
+        event: `특수강간 불기소, 전 법무부 장관 김학의 성폭력 사건(검찰)`
     }, {
-        quote: `박씨가 피해 여성의 의사를 무시하고 성관계를 한 것은 인정된다. ... 다만 피고인이 상대방의 반항을 현저하게 곤란할 정도로 폭행⋅협박하지 않았다`,
-        event: `감자탕 성폭행 사건`
+        quote: `피해 여성의 의사를 무시하고 성관계를 한 것은 인정된다. (...) 다만 피 고인이 상대방의 반항을 현저하게 곤란할 정도로 폭행⋅협박하지 않았다`,
+        event: `강간 무죄, 감자탕 성폭력 사건(1심)`
     }, {
-
+        quote: `강간할 의도를 가지고 있었다고 인정하려면 폭행이나 협박을 가한 정황 이 입증돼야 하지만, 이를 인정할 증거가 부족하다`,
+        event: `강간미수 무죄, 신림동 스토킹 사건(1심 및 2심)`
+    }, {
+        quote: `6개월 동안 피해가 지속되었지만 피해자가 적극 저항한 증거가 없다 (...) 피고인이 피해자의 항거를 불가능하게 하거나 현저히 곤란하게 할 정도 의 유형력이 없었다`,
+        event: `강간 무죄, 형부에 의한 이주여성 성폭력 사건(1심)`
+    }, {
+        quote: `상대방의 몸을 누르거나 팔을 잡는 행위는 성관계를 시작하면서 수반되는 일반적인 동작이어서 위와 같은 행위가 있었다는 사정만으로 강간의 수단인 폭행이 인정된다고 단정하기 어렵다`,
+        event: `강간치상 무죄, 해군 상관에 의한 성소수자 여군 성폭력 사건(2심)`
+    }, {
+        quote: `사회통념상 강간 피해를 당한 직후의 여성의 행동으로 이해하기 어렵다. (...) 항거를 불가능하게 할 정도로 폭행, 협박해 간음한 사실이 증명됐다고 보기 어렵다`,
+        event: `강간 무죄, 술을 이용한 포장마차 성폭력 사건(1심)`
     }]
 
     const raws = [{
-        title: ``,
+        title: `폭행이나 협박 또는 사람의 의사에 반하여 해당하는 사람을 강간한 자는 3년 이상의 유기징역에 처한다`,
         status: `계류`,
     }, {
-        title: `의제강간 연령 현생 13세 미만에서 상향`,
+        title: `상대방의 명백한 동의가 없는 상태에서 사람을 간음한 사람은 5년 이상 의 유기징역에 처한다`,
         status: `계류`,
     }, {
-        title: ` ‘사람의 의사에 반하는 경우’를 성폭력 범죄 요건에 포함`,
+        title: `상대방의 의사에 반하여 사람을 간음한 사람은 3년 이상의 유기징역에 처한다`,
+        status: `계류`,
+    }, {
+        title: `동의 없이 사람을 간음한 사람은 3년 이하의 징역 또는 1천만원 이하의 벌금에 처한다`,
+        status: `계류`,
+    }, {
+        title: `폭행이나 협박 또는 상대방의 동의 없이 사람을 강간한 자는 3년 이상의 유기징역에 처한다`,
+        status: `계류`,
+    }, {
+        title: `상대방의 부동의 의사에 반하여 사람을 간음한 자는 3년 이상의 유기징역에 처한다`,
+        status: `계류`,
+    }, {
+        title: `사람의 명백한 거부의사표시에 반하여 강간한 사람은 1년 이상의 유기징역에 처한다`,
+        status: `계류`,
+    }, {
+        title: `동의 없이 사람을 간음한 사람은 3년 이상의 유기징역에 처한다`,
         status: `계류`,
     }]
 
     return (
         <div className={style.events}>
-            <Ticker speed={5}>
+            {/*<div className={layout.container}>*/}
+            {/*    <h3>제20대 국회 법안</h3>*/}
+            {/*</div>*/}
+            <Ticker speed={4} direction={'toRight'}>
                 {(props) => {
                     return (
                         <ul style={{display: 'flex'}}>
@@ -75,12 +99,12 @@ export default () => {
                     )
                 }}
             </Ticker>
-            <Ticker speed={3}>
+            <Ticker speed={2}>
                 {(props) => {
                     return (
                         <ul style={{display: 'flex'}}>
                             {events.map((event, i) =>
-                                <li key={`event-${i}`} style={{width: '320px'}}>
+                                <li key={`event-${i}`} style={{width: '360px', maxWidth: '100%'}}>
                                     <Event event={event} />
                                 </li>
                             )}
@@ -88,19 +112,19 @@ export default () => {
                     )
                 }}
             </Ticker>
-            <Ticker direction={'toRight'} speed={2}>
-                {(props) => {
-                    return (
-                        <ul style={{display: 'flex'}}>
-                            {quotes.map((quote, i) =>
-                                <li key={`raw-${i}`} style={{width: '320px'}}>
-                                    <Quote quote={quote} />
-                                </li>
-                            )}
-                        </ul>
-                    )
-                }}
-            </Ticker>
+            {/*<Ticker direction={'toRight'} speed={2}>*/}
+            {/*    {(props) => {*/}
+            {/*        return (*/}
+            {/*            <ul style={{display: 'flex'}}>*/}
+            {/*                {quotes.map((quote, i) =>*/}
+            {/*                    <li key={`raw-${i}`} style={{width: '320px'}}>*/}
+            {/*                        <Quote quote={quote} />*/}
+            {/*                    </li>*/}
+            {/*                )}*/}
+            {/*            </ul>*/}
+            {/*        )*/}
+            {/*    }}*/}
+            {/*</Ticker>*/}
 
         </div>
     )
