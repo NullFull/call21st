65d5c7e2e0e4e4fa5e40e677857ff39247b01048
lexifdev <sl@lxf.kr>
2020-04-01 15:48:59 +0900

    DB 접근 정보 환경변수 이용하도록

--- stores/_db.js
+++ stores/_db.js
@@ -1,11 +1,21 @@
 import admin from 'firebase-admin'
-import serviceAccount from 'keys/call21st-works-4fc84944bff6.json'
 
 
 const getDB = () => {
     if (!admin.apps.length) {
         admin.initializeApp({
-            credential: admin.credential.cert(serviceAccount)
+            credential: admin.credential.cert({
+                "type": "service_account",
+                "project_id": process.env.FIREBASE_PROJECT_ID,
+                "private_key_id": process.env.FIREBASE_PKEY_ID,
+                "private_key": process.env.FIREBASE_PKEY,
+                "client_email": process.env.FIREBASE_EMAIL,
+                "client_id": process.env.FIREBASE_CLIENT_ID,
+                "auth_uri": "https://accounts.google.com/o/oauth2/auth",
+                "token_uri": "https://oauth2.googleapis.com/token",
+                "auth_provider_x509_cert_url": "https://www.googleapis.com/oauth2/v1/certs",
+                "client_x509_cert_url": "https://www.googleapis.com/robot/v1/metadata/x509/firebase-adminsdk-nqr4j%40call21st-works.iam.gserviceaccount.com"
+            })
         })
     }
 
