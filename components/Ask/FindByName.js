08d47b7c78e9647cfad0f01eac90efaf9dd59e6b
constmoon <jiiw.kang@gmail.com>
2020-04-01 15:47:03 +0900

    이름으로 검색시 엔터키 지원하도록 추가

--- components/Ask/FindByName.js
+++ components/Ask/FindByName.js
@@ -5,9 +5,15 @@ import style from './FindByName.styl'
 const FindByName = ({onSubmit}) => {
     const [q, setQ] = React.useState('')
 
+    const handleKeyPress = e => {
+        if (e.keyCode === 13) {
+            onSubmit(q)
+        }
+    }
+
     return (
         <div className={style.wrapper}>
-            <input placeholder="이름으로 검색" value={q} onChange={e => setQ(e.target.value)}/>
+            <input placeholder="이름으로 검색" value={q} onChange={e => setQ(e.target.value)} onKeyDown={e => handleKeyPress(e)} />
             <button disabled={q.length < 1} onClick={e => onSubmit && onSubmit(q)}>검색</button>
         </div>
     )
