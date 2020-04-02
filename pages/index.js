91c1aa0fb97aa0f7e32d22905686bc46c0f48be0
lexifdev <sl@lxf.kr>
2020-04-02 09:16:58 +0900

    인덱스 스타일 변경

--- pages/index.js
+++ pages/index.js
@@ -15,26 +15,27 @@ export default () => {
     return (
         <>
             <Head>
-                <title>#21대 국회에 요구한다</title>
+                <title>21대 국회에 요구한다</title>
             </Head>
+
             <div style={{textAlign: 'right'}}>
-                <h3>#CALL21ST <img src="" href="#" /><button>공유</button></h3>
+                <h3>#CALL21ST</h3>
             </div>
-            <hr />
+
             <section className={cx(style.odd, style.lead)}>
                 <div className={layout.container}>
                     <h2>나는 오늘 ‘성평등’에 투표합니다.</h2>
-                    <h1><span>강간죄 판단 기준을</span> <span>‘동의’ 여부로 바꾸는 데 찬성하십니까?</span></h1>
+                    <h1><span>강간죄 판단 기준을</span> <span>‘동의’ 여부로 바꾸는 데</span> <span>찬성하십니까?</span></h1>
 
-                    <p>텔레그램 성착취방 26만 명 이용, 성폭력 해도 쉽게 풀려나고 용서받는 강간문화, 비난·조롱받으며 찬밥신세된 미투 법안들, 21대 국회는 바꿀 수 있습니까?</p>
+                    <p>텔레그램 성착취방 26만 명 이용,<br/> 성폭력 해도 쉽게 풀려나고 용서받는 강간문화,<br/> 비난·조롱받으며 찬밥신세된 미투 법안들,<br/> 21대 국회는 바꿀 수 있습니까?</p>
 
                     <p>당신의 작은 참여가 성평등한 미래를 만듭니다. 지금 후보에게 질문을 보내보세요.</p>
 
-                    <div>
-                        <button>후보에게 질문하기</button>
-                        |
-                        <button>답변보기</button>
-                    </div>
+                    {/*<div>*/}
+                    {/*    <button>후보에게 질문하기</button>*/}
+                    {/*    |*/}
+                    {/*    <button>답변보기</button>*/}
+                    {/*</div>*/}
 
                     {/*<p>*/}
                     {/*    가해자대신 변명해주는 법 아닌 가해자를 제대로 처벌하는 법<br/>*/}
