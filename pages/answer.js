6ec0c887aba7183dc8f02641764bd24e7da2114d
lexifdev <sl@lxf.kr>
2020-04-15 18:20:29 +0900

    질답 시간 종료

--- pages/answer.js
+++ pages/answer.js
@@ -3,6 +3,7 @@ import client from 'utils/client'
 import layout from './layout.styl'
 import style from './answer.styl'
 
+
 const LoadingSpinner = () => {
     return (
         <div className={style.loader}>
@@ -10,6 +11,7 @@ const LoadingSpinner = () => {
         </div>
     )
 }
+
 
 export default ({url: {query}}) => {
     const [choice, setChoice] = React.useState(null)
@@ -46,38 +48,36 @@ export default ({url: {query}}) => {
             </div>
             <hr />
             <div className={layout.container}>
-                <h3>강간죄 구성 요건을 '동의'여부로 바꾸는데 동의하십니까?</h3>
-                <div style={{margin: '6px 0', fontSize: '1.2rem'}}>
-                    <label>
-                        <input
-                            type="radio"
-                            value={'yes'}
-                            checked={choice === 'yes'}
-                            onChange={e => setChoice(e.target.value)}
-                        />
-                        동의합니다.
-                    </label>
-                </div>
-                <div style={{margin: '6px 0', fontSize: '1.2rem'}}>
-                    <label>
-                        <input
-                            type="radio"
-                            value={'no'}
-                            checked={choice === 'no'}
-                            onChange={e => setChoice(e.target.value)}
-                        />
-                        동의하지 않습니다.
-                    </label>
-                </div>
+                <h3>답변 할 수 있는 시간이 지났습니다.</h3>
+                {/*<h3>강간죄 구성 요건을 '동의'여부로 바꾸는데 동의하십니까?</h3>*/}
+                {/*<div style={{margin: '6px 0', fontSize: '1.2rem'}}>*/}
+                {/*    <label>*/}
+                {/*        <input*/}
+                {/*            type="radio"*/}
+                {/*            value={'yes'}*/}
+                {/*            checked={choice === 'yes'}*/}
+                {/*            onChange={e => setChoice(e.target.value)}*/}
+                {/*        />*/}
+                {/*        동의합니다.*/}
+                {/*    </label>*/}
+                {/*</div>*/}
+                {/*<div style={{margin: '6px 0', fontSize: '1.2rem'}}>*/}
+                {/*    <label>*/}
+                {/*        <input*/}
+                {/*            type="radio"*/}
+                {/*            value={'no'}*/}
+                {/*            checked={choice === 'no'}*/}
+                {/*            onChange={e => setChoice(e.target.value)}*/}
+                {/*        />*/}
+                {/*        동의하지 않습니다.*/}
+                {/*    </label>*/}
+                {/*</div>*/}
 
-                <div style={{marginTop: '20px'}}>
-                    <button className={style.submit} onClick={() => response()}>저장</button>
-                </div>
+                {/*<div style={{marginTop: '20px'}}>*/}
+                {/*    <button className={style.submit} onClick={() => response()}>저장</button>*/}
+                {/*</div>*/}
                 {loading && <LoadingSpinner />}
             </div>
-            
         </div>
     )
 }
-
-
--- pages/api/requests.js
+++ pages/api/requests.js
@@ -5,11 +5,15 @@ import {makeResponse} from 'utils/api'
 export default async (req, res) => {
     const { to, content } = req.body
 
-    await Promise.all(
-        to.map(item => requestStore.create(item, content))
-    )
-
     makeResponse(res, {
-
+        message: '지금은 질문할 수 없습니다'
     })
+
+    // await Promise.all(
+    //     to.map(item => requestStore.create(item, content))
+    // )
+    //
+    // makeResponse(res, {
+    //
+    // })
 }
